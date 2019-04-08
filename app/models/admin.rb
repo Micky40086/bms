7f01214e857de40dc4bef619b86f7db98e019ac9
Mickey <Micky40086@gmail.com>
2019-04-08 09:14:02 +0800

    feat: google oauth2

---
+++ app/models/admin.rb
@@ -0,0 +1,20 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    admin = Admin.where(email: data["email"]).first
+
+    # Uncomment the section below if you want admins to be created if they don't exist
+    unless admin
+      admin = Admin.create(name: data["name"],
+                           email: data["email"],
+                           password: Devise.friendly_token[0, 20],
+                           provider: access_token.provider)
+    end
+    admin
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {omniauth_callbacks: "admins/omniauth_callbacks"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "admin#home"
 end
